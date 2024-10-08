c84ed0a17703b1315ddbab7771fe879a869a0990
zidaneho <zidaneho4567@gmail.com>
2024-10-08 01:38:42 -0700

    setup 5

---
+++ TritonWare30UnityProject/Assets/Scripts/FuncPredicate.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class FuncPredicate : IPredicate {
+    readonly Func<bool> func;
+        
+    public FuncPredicate(Func<bool> func) {
+        this.func = func;
+    }
+        
+    public bool Evaluate() => func.Invoke();
+}
--- TritonWare30UnityProject/Assets/Scripts/IState.cs
+++ TritonWare30UnityProject/Assets/Scripts/IState.cs
@@ -1,7 +1,53 @@
 public interface IState
 {
+    public void OnAwake();
     public void OnEnter();
     public void OnUpdate();
     public void OnFixedUpdate();
     public void OnExit();
+}
+
+public interface ITransition {
+    IState To { get; }
+    IPredicate Condition { get; }
+}
+public interface IPredicate {
+    bool Evaluate();
+}
+public class Transition : ITransition {
+    public IState To { get; }
+    public IPredicate Condition { get; }
+
+    public Transition(IState to, IPredicate condition) {
+        To = to;
+        Condition = condition;
+    }
+}
+public class BaseMonsterState : IState
+{
+    
+    public void OnAwake()
+    {
+        
+    }
+
+    public void OnEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnFixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExit()
+    {
+        throw new System.NotImplementedException();
+    }
 }
