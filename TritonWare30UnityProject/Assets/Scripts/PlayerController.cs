bc9a51af8f8bfe22f0bb48f61bf156a1241a6dac
OrigamiStarz <40199724+OrigamiStarz@users.noreply.github.com>
2024-10-08 16:18:23 -0700

    Add stamina system

--- TritonWare30UnityProject/Assets/Scripts/PlayerController.cs
+++ TritonWare30UnityProject/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
     
     private Rigidbody2D _rigidbody;
     private InputBank _input;
+    [SerializeField] private float _stamina; // max: 10, for now
+    private bool isRunning;
 
     private void Awake()
     {
@@ -28,17 +30,55 @@ public class PlayerController : MonoBehaviour
         _currentSpeed = walkSpeed;
     }
 
+    private void Start()
+    {
+        _stamina = 10;
+    }
+    
     private void Update()
     {
-        if (_input.isRunPressed) _currentSpeed = runSpeed;
-        else _currentSpeed = walkSpeed;
+        // for debug purposes
+        
+        
+        // change from walk/run
+        if (_input.isRunPressed && _stamina > 0)
+        {
+            _currentSpeed = runSpeed;
+            isRunning = true;
+        }
+        else
+        {
+            _currentSpeed = walkSpeed;
+            isRunning = false;
+        }
+        
+        // if the character is running, consume stamina
+        if (isRunning)
+        {
+            _stamina = Math.Max(_stamina - Time.deltaTime, 0);
+            // stop running if character runs out of stamina
+            if (_stamina == 0)
+            {
+                isRunning = false;
+            }
+        }
+        // restore stamina back to max if not running
+        else
+        {
+            _stamina = Math.Min(_stamina + (Time.deltaTime)/2, 10);
+        }
     }
 
     private void FixedUpdate()
     {
+        // move the player
         Vector2 newPos = (Vector2)transform.position + _currentSpeed * Time.deltaTime * _input.moveVector;
         _rigidbody.MovePosition(newPos);
     }
 
+    public int getStamina()
+    {
+        return (int) _stamina;
+    }
     
 }
