a70ed8f8953bd9e0a2d7d6a844c1dfd4ce5d6537
zidaneho <zidaneho4567@gmail.com>
2024-10-20 14:39:37 -0700

    fmod, new sprites, sync some sounds

--- TritonWare30UnityProject/Assets/Scripts/Interactable/Battery.cs
+++ TritonWare30UnityProject/Assets/Scripts/Interactable/Battery.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEditor.DeviceSimulation;
 using UnityEngine;
 
 public class Battery : MonoBehaviour, IInteractable
 {
     [SerializeField] private float batteryAmount = 5f;
+    [SerializeField] private EventReference pickupSoundEffect;
     public string popupDescription => "Pickup";
 
     // When clicked on (e or mouse click)
@@ -17,6 +19,8 @@ public class Battery : MonoBehaviour, IInteractable
         // restore flashlight (do not go over max)
         flashlightComponent.increaseBattery(batteryAmount);  // will not go over max
         Debug.Log("Battery increased to: " + flashlightComponent.getBattery());
+        
+        Util.PlaySound(pickupSoundEffect.Path,gameObject);
         
         // make it disappear
         Destroy(gameObject);
--- TritonWare30UnityProject/Assets/Scripts/Interactable/WorldKey.cs
+++ TritonWare30UnityProject/Assets/Scripts/Interactable/WorldKey.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using FMODUnity;
 using UnityEngine;
 
 public class WorldKey : MonoBehaviour, IInteractable
 {
+    [SerializeField] private EventReference pickupSoundEffect;
     public string popupDescription => "Pickup";
     public void OnInteract(GameObject interactor)
     {
         var keyComponent = interactor.GetComponent<KeyComponent>();
         if (keyComponent != null) keyComponent.AddKey();
+        Util.PlaySound(pickupSoundEffect.Path,gameObject);
         Destroy(gameObject);
     }
 }
